04010cc6a657894ba43e984a5e80949defd071a6
cnarendhar <narendhar.chiluka@bandgitechnologies.com>
2018-09-05 13:58:04 +0530

    chore(release): Release patch version (1.11.17)

--- src/app/main/sample/sample.controller.js
+++ src/app/main/sample/sample.controller.js
@@ -7,15 +7,40 @@
         .controller('SampleController', SampleController);
 
     /** @ngInject */
-    function SampleController(SampleData)
+    function SampleController($scope, $http, $location)
     {
-        var vm = this;
+        //var vm = this;
 
         // Data
-        vm.helloText = SampleData.data.helloText;
+        //vm.helloText = SampleData.data.helloText;
 
         // Methods
 
         //////////
+        $http.get("https://api.github.com/repos/narendhar11/gulp-release/releases?access_token=ff72e13727e83506b7407643fc5fabff4650decb")
+        .then(function(response) {
+            $scope.myWelcome = response.data;            
+            if ($location.host() == 'conedmms.cniguard.com') {
+                angular.forEach($scope.myWelcome, function(value){
+                    if(value.prerelease == false){
+                        $scope.env = 'Production'
+                        $scope.releaseVersion = value.tag_name;
+                    }                    
+                });
+            }else{
+                angular.forEach($scope.myWelcome, function(value){
+                    if(value.prerelease == true){
+                        $scope.env = 'Test'
+                        $scope.releaseVersion = value.tag_name;
+                    }                    
+                });
+            }
+            
+            
+            
+            
+        });
+
+        
     }
 })();
